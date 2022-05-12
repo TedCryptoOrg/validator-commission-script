61693d056f0b7c9ddbd2b4c6d78e8eeaaaee1aa7
Jose Lopes <josluis.lopes@gmail.com>
2022-05-12 17:33:30 +0100

    Allow tokens to be sent to external wallet

--- claim_rewards.py
+++ claim_rewards.py
@@ -6,17 +6,20 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # ENVIRONMENT SETTINGS
-binary = os.environ.get('binary')
-validator_wallet = os.environ.get('validator_wallet')
-node = os.environ.get('node')
-validator_address = os.environ.get('validator_address')
-password = os.environ.get('keyring_password')
-chain_id = os.environ.get('chain_id')
-wallet_name = os.environ.get('keyring_wallet_name')
-denom = os.environ.get('denom')
-chain_name = os.environ.get('chain_name')
-restake_percentage = os.environ.get('restake_percentage')
-min_stake_balance = float(os.environ.get('min_stake_balance'))
+binary = os.environ.get('BINARY')
+validator = os.environ.get('VALIDATOR')
+keyring_wallet_name = os.environ.get('KEYRING_WALLET_NAME')
+keyring_password = os.environ.get('KEYRING_PASSWORD')
+chain_name = os.environ.get('CHAIN_NAME')
+chain_id = os.environ.get('CHAIN_ID')
+denom = os.environ.get('DENOM')
+node = os.environ.get('NODE')
+restake_min_balance = float(os.environ.get('RESTAKE_MIN_BALANCE'))
+restake_wallet_address = os.environ.get('RESTAKE_WALLET_ADDRESS')
+restake_wallet_percentage = float(os.environ.get('RESTAKE_WALLET_PERCENTAGE'))
+external_min_balance = float(os.environ.get('EXTERNAL_MIN_BALANCE'))
+external_wallet_address = os.environ.get('EXTERNAL_WALLET_ADDRESS')
+external_wallet_percentage = float(os.environ.get('EXTERNAL_WALLET_PERCENTAGE'))
 
 print('===== Running script =======')
 
@@ -26,11 +29,9 @@ def get_mintscan_url(tx):
     return 'https://www.mintscan.io/' + chain_name + '/txs/' + tx
 
 
-# GET WALLET BALACNE
+# GET WALLET BALANCE
 def get_wallet_balance(wallet_address, node = None):
-    command = binary + ' query bank balances ' + wallet_address + ' -o json'
-    if node:
-        command += ' --node ' + node
+    command = binary + ' query bank balances ' + wallet_address + ' --node ' + node + ' -o json'
     result = subprocess.run(command, shell=True, check=True, capture_output=True)
 
     if result.returncode != 0:
@@ -71,11 +72,11 @@ def run_command(command):
 
 # CLAIM REWARDS
 def claim_rewards(gas_fees):
-    command = 'echo -e "' + password + "\n" + password + '\n" | ' \
-              + binary + ' tx distribution withdraw-rewards ' + validator_address \
+    command = 'echo -e "' + keyring_password + "\n" + keyring_password + '\n" | ' \
+              + binary + ' tx distribution withdraw-rewards ' + restake_wallet_address \
               + ' --chain-id ' + chain_id \
               + ' --node ' + node \
-              + ' --from ' + wallet_name \
+              + ' --from ' + keyring_wallet_name \
               + ' --commission' \
               + ' -y --fees ' + str(gas_fees) + denom
 
@@ -83,18 +84,31 @@ def claim_rewards(gas_fees):
 
 
 # STAKE FUNCTION
-def stake(validator_address, stake_balance, gas_fees):
-    command = 'echo -e "' + password + "\n" + password + '\n" | ' \
-              + binary + ' tx staking delegate ' + validator_address + ' ' + stake_balance + denom \
+def stake(stake_balance, gas_fees):
+    command = 'echo -e "' + keyring_password + "\n" + keyring_password + '\n" | ' \
+              + binary + ' tx staking delegate ' + validator + ' ' + stake_balance + denom \
               + ' --chain-id ' + chain_id \
               + ' --node ' + node \
-              + ' --from ' + wallet_name \
+              + ' --from ' + keyring_wallet_name \
               + ' -y --fees ' + str(gas_fees) + denom
 
     return run_command(command)
 
 
-original_validator_balance = get_wallet_balance(validator_wallet, node)
+# Send tokens between wallets
+def send_token(amount, gas_fees):
+    command = 'echo -e "' + keyring_password + "\n" + keyring_password + '\n" | ' \
+              + binary + ' tx bank send ' + restake_wallet_address + ' ' + str(amount) \
+              + ' ' + str(amount) + denom \
+              + ' --chain-id ' + chain_id \
+              + ' --node ' + node \
+              + ' --from ' + keyring_wallet_name \
+              + ' -y --fees ' + str(gas_fees) + denom
+
+    return run_command(command)
+
+
+original_validator_balance = get_wallet_balance(restake_wallet_address)
 
 print("Validator balance:" + str(original_validator_balance))
 
@@ -107,7 +121,7 @@ print('Waiting for it to be accepted...')
 
 time.sleep(10)
 
-balance = get_wallet_balance(validator_wallet, node)
+balance = get_wallet_balance(restake_wallet_address)
 if original_validator_balance == balance:
     print('Validator balance was not updated. Commission probably wasn\'t accepted? Please check TX', command_result)
     exit(1)
@@ -115,11 +129,20 @@ if original_validator_balance == balance:
 print(' -- Claim Successful -- ')
 
 balance -= 1000000
-restake_balance = balance*float(restake_percentage)/100
+restake_amount = balance*restake_wallet_percentage/100
+external_amount = balance*external_wallet_percentage/100
 
-print('Your safe stake balance is "'+ str(balance) + denom + '"... You are going to stake ' + str(restake_percentage) + ' which is ' + str(restake_balance) + denom)
+print('Your workable (balance - 1token for fees) is ' + str(balance))
 
-if restake_balance > min_stake_balance:
-    stake(validator_address, restake_balance, 2500)
-else:
-    print('Re-stake balance does not meet your min stake balance')
+if restake_amount > 0.001:
+    print('You possible restake amount is ' + str(restake_amount) + denom)
+if external_amount > 0.001:
+    print('Your possible external amount is ' + str(external_amount) + denom)
+
+if restake_amount < restake_min_balance or external_amount < external_min_balance:
+    print('Minimums are not. Min restake balance is set to ' + str(restake_min_balance) + ' and external min balance is set to ' + str(external_min_balance))
+    exit(0)
+
+stake(restake_amount, 2500)
+time.sleep(10)
+send_token(external_amount, 2500)
